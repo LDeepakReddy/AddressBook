e68c7f4336d2d9e2ae8b23a7cc7980501831757a
LDeepakReddy <l.deepakreddy.com>
2022-03-05 00:47:25 +0530

    ADDED: Welcome message

---
+++ src/com/blz_addressbook/AddressBookMain.java
@@ -0,0 +1,8 @@
+package com.blz_addressbook;
+
+public class AddressBookMain {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address book program");
+    }
+}
