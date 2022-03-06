7be3251af69e69ef59d3a375f8db24429289fc77
LDeepakReddy <l.deepakreddy.com>
2022-03-07 00:13:41 +0530

    Added:Contact modification feature

--- src/com/blz_addressbook/AddressBook.java
+++ src/com/blz_addressbook/AddressBook.java
@@ -5,12 +5,12 @@ import java.util.List;
 import java.util.Scanner;
 
 public class AddressBook {
-    public static List<Contacts> Contacts=new ArrayList<>();
-    Scanner Sc=new Scanner(System.in);
+    public static List<Contacts> Contacts = new ArrayList<>();
+    Scanner Sc = new Scanner(System.in);
 
-    public void addContacts(){
+    public void addContacts() {
         System.out.println("Enter the contact Information");
-        Contacts contacts=new Contacts();
+        Contacts contacts = new Contacts();
 
         System.out.println("Enter the First Name ");
         contacts.setFirstName(Sc.next());
@@ -38,4 +38,52 @@ public class AddressBook {
 
         Contacts.add(contacts);
     }
+
+    public void modifyContacts(String modify) {
+        for (int i = 0; i < Contacts.size(); i++) {
+            Contacts contacts = (Contacts) Contacts.get(i);
+            if (contacts.getFirstName().equals(modify)) {
+                System.out.println("Which data you want to edit?");
+                System.out.println("1.Edit First name");
+                System.out.println("2.Edit Last name ");
+                System.out.println("3.Edit Address");
+                System.out.println("4.Edit Phone number");
+                System.out.println("5.Edit City name");
+                System.out.println("6.Edit Zip number");
+                System.out.println("7.Edit Email address");
+
+                switch (Sc.nextInt()) {
+                    case 1:
+                        contacts.setFirstName(Sc.next());
+                        break;
+                    case 2:
+                        contacts.setLastName(Sc.next());
+                        break;
+                    case 3:
+                        contacts.setAddress(Sc.next());
+                        break;
+                    case 4:
+                        contacts.setPhoneNumber(Sc.nextLong());
+                        break;
+                    case 5:
+                        contacts.setCity(Sc.next());
+                        break;
+                    case 6:
+                        contacts.setZip(Sc.nextInt());
+                        break;
+                    case 7:
+                        contacts.setEmail(Sc.next());
+                        break;
+
+                }
+
+
+            }
+
+            System.out.println(AddressBook.Contacts);
+        }
+    }
+
 }
+
+
--- src/com/blz_addressbook/AddressBookMain.java
+++ src/com/blz_addressbook/AddressBookMain.java
@@ -6,15 +6,35 @@ public class AddressBookMain {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Address book program");
-        Scanner Sc=new Scanner(System.in);
-        AddressBook addressBook =new AddressBook();
-        System.out.println("How many contacts you want to add");
-        int contactsCount =Sc.nextInt();
-        for (int i = 0; i< contactsCount;i++){
-            addressBook.addContacts();
-        }
-        for (int i=0;i<contactsCount;i++){
-            System.out.println(AddressBook.Contacts.get(i));
+        System.out.println("1. Add contacts to the addressbook.");
+        System.out.println("2. Modify the contact");
+        System.out.println("3. Delete the contact");
+        AddressBook addressBook = new AddressBook();
+        Scanner Sc = new Scanner(System.in);
+        int operations = Sc.nextInt();
+        while (operations != 0) {
+            switch (operations) {
+                case 1:
+                    System.out.println("How many contacts you want to add");
+                    int contactsCount = Sc.nextInt();
+                    for (int i = 0; i < contactsCount; i++) {
+                        addressBook.addContacts();
+                    }
+                    for (int i = 0; i < contactsCount; i++) {
+                        System.out.println(AddressBook.Contacts.get(i));
+                    }
+                    break;
+                case 2:
+                    System.out.println("Enter the contact which you want to modify");
+                    String check = Sc.next();
+                    addressBook.modifyContacts(check);
+                    break;
+            }
+            System.out.println("1. Add contacts to the addressbook.");
+            System.out.println("2. Modify the contact");
+            System.out.println("3. Delete the contact");
+            operations = Sc.nextInt();
         }
     }
+
 }
